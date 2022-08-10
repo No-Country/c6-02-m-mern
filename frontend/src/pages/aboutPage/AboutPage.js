e43ab8c9b5bee925881e0d1969e66eabeb0afda5
cristian mosquera <cmosqbone1@gmail.com>
2022-08-10 17:53:57 +0200

    prueba

--- frontend/src/pages/aboutPage/AboutPage.js
+++ frontend/src/pages/aboutPage/AboutPage.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import './aboutPage.css';
 
 export const AboutPage = () => {
-  return <div className="AboutPage">AboutPage</div>;
+  return (
+    <div className="aboutPage">
+      <h2>aboutPage</h2>
+      <hr />
+    </div>
+  );
 };
