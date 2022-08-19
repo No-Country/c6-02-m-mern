b246aa798ffc7dcf604a42fa9a51135af05fd651
unknown <santiagoschroh@gmail.com>
2022-08-19 10:39:57 -0300

    CSS reparado

--- frontend/src/components/DesingPageComponents/DownContainer/Bar/Back.js
+++ frontend/src/components/DesingPageComponents/DownContainer/Bar/Back.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
-import '../DownContainer/Bar/Progress.css'
-import '../DownContainer/NextBack.css'
-import {Circle} from '../DownContainer/Bar/Circle'
+import { Circle } from "./Circle";
+import '../NextBack.css'
+import './Bar'
+import './Progress.css'
 
 export const Back = () => {
+    
+      
     const[circle] = useState(3);
     const [active, setActive] = useState(0);
     const [width, setWidth] = useState(0);
@@ -14,12 +17,23 @@ export const Back = () => {
     for (let i = 0; i < circle; i++) {
            arr.push( <Circle className={i<=active?"circle active":"circle"} key={i}>{i}</Circle>) 
     }
+    const handlerButtonBack = () => {
+        active<=0?setActive(0):setActive(active-1)
+        if(active <= 0){
+            setActive(0)
+        } else {
+            setActive(active-1)
+        }
+    }
+
+    
     return(
        
  
-         
-        <button className="cssButton" disabled = {active>0?false:true} onClick={() =>{active<=0?setActive(0):setActive(active-1)}}>BACK</button> 
-
+        
+        <button className="cssButton" onClick={handlerButtonBack}  disabled = {active>0?false:true}> BACK </button>
+      
+    
       
  
     )
--- frontend/src/components/DesingPageComponents/DownContainer/Bar/Bar.js
+++ frontend/src/components/DesingPageComponents/DownContainer/Bar/Bar.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Circle } from "./Circle";
 import './Progress.css'
 import '../NextBack.css'
-import { Link, useNavigate } from "react-router-dom";
 
 
 
-export const Bar = () => {
-    const navigate = useNavigate()
+
+export const Bar = (Back) => {
+    
     const[circle] = useState(3);
     const [active, setActive] = useState(0);
     const [width, setWidth] = useState(0);
@@ -18,22 +18,40 @@ export const Bar = () => {
     for (let i = 0; i < circle; i++) {
            arr.push( <Circle className={i<=active?"circle active":"circle"} key={i}>{i}</Circle>) 
     }
-    return(
-        <div className="containera">
-               <div className="buttonContainer">
+    const handlerButtonBack = () => {
+        active<=0?setActive(0):setActive(active-1)
+        if(active <= 0){
+            setActive(0)
+        } else {
+            setActive(active-1)
+        }
+    }
 
-                    <button onClick={() =>{active<=0?setActive(0):setActive(active-1) && navigate('/')}} className="cssButton" disabled = {active>0?false:true} >BACK</button> 
-                    <button className="cssButton" disabled = {active>=circle-1?true:false} onClick={() =>{active>=circle?setActive(circle+1):setActive(active+1)}}>NEXT</button> 
+    const handlerButtonNext = () => {
+        if(active >= circle) {
+            setActive(circle+1)
+        } else {
+            setActive(active+1)
+        }
+    }
+
+    return(
+        
+                <div className="buttonContainer">
+
+                    <button onClick={handlerButtonBack} className="cssButton" disabled = {active>0?false:true} >BACK</button> 
                     
+                        <div className="progressbar">
+                            <div className="progress" style={{width:width+"%"}}></div>
+                                {arr}
+                        </div>
+                    <button onClick={handlerButtonNext}  className="cssButton" disabled = {active>=circle-1?true:false} >NEXT</button> 
+
                 </div>
-            <div className="content">
-                <div className="progressbar">
-                    <div className="progress" style={{width:width+"%"}}></div>
-                    {arr}
-                </div>
-             
-            </div>
-        </div>
+           
+              
+            
+      
     )
 }
 
