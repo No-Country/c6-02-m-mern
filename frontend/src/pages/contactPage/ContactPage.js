f808b93f0a43f7afae832dff3b21e911a37b480b
cristian mosquera <cmosqbone1@gmail.com>
2022-08-25 16:11:30 +0200

    adding header to the contact page

--- frontend/src/pages/contactPage/ContactPage.js
+++ frontend/src/pages/contactPage/ContactPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { HeaderSub } from "../../components/DesingPageComponents/HeaderSub/HeaderSub";
 import { SectionContact } from "../../components/LandingPageComponents/contact/SectionContact";
 
 export const ContactPage = () => {
   return (
     <div className="ContactPage">
+      <HeaderSub />
       <SectionContact />
     </div>
   );
