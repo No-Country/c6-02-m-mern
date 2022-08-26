4b2902213471621bd1724764dae20f976cd466ae
cristian mosquera <cmosqbone1@gmail.com>
2022-08-26 11:10:42 +0200

    reusing components

--- frontend/src/pages/desingpacksPage/DesingPacksPage.js
+++ frontend/src/pages/desingpacksPage/DesingPacksPage.js
@@ -1,35 +1,15 @@
 import { HeaderSub } from "../../components/DesingPageComponents/HeaderSub/HeaderSub.js";
 import { NextAndBack } from "../../components/DesingPageComponents/DownContainer/NextBack.js";
-import CardDesktop from "../../components/DesingPageComponents/Cards/CardDesktop";
-import Carousel from "../../components/DesingPageComponents/Cards/Carousel";
-import Titulo from "../../components/DesingPageComponents/Cards/Titulo";
+import { SectionDesktopCards } from "../../components/DesingPageComponents/Cards/SectionDesktopCards.js";
 import "./desingpacksPakcs.css";
-import Question from "../../components/DesingPageComponents/Cards/Question";
 
 export const DesingPacksPage = () => {
   return (
     <div className="desingPacksPage">
       <HeaderSub />
       <div className="container custom-flexDesingpage  ">
-        <div className="row custom-desktop">
-          <div className="col-12 ">
-            <div className="col align-self-center">
-              <Titulo />
-            </div>
-          </div>
-
-          <div className="col-12 d-lg-none d-md-block">
-            <Carousel />
-          </div>
-          <div className="col align-self-start custom-question">
-            <Question />
-          </div>
-          <div className="col-12 d-none d-lg-block ">
-            <CardDesktop />
-          </div>
-        </div>
+        <SectionDesktopCards />
       </div>
-
       <NextAndBack />
     </div>
   );
