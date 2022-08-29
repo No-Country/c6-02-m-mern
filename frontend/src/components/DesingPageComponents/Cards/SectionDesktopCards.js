649c6d1498b209aaae83971f1fce62caf6fdba13
cristian mosquera <cmosqbone1@gmail.com>
2022-08-29 19:48:53 +0200

    adding the routes and the styles and adding the functionality too

--- frontend/src/components/DesingPageComponents/Cards/SectionDesktopCards.js
+++ frontend/src/components/DesingPageComponents/Cards/SectionDesktopCards.js
@@ -1,36 +1,12 @@
-import React from "react";
-import { ModalQuestion } from "../ModalQuestion.js/ModalQuestion";
-import CardDesktop from "./CardDesktop";
-import Carousel from "./Carousel";
-import Question from "./Question";
-import Titulo from "./Titulo";
+// import React from "react";
+// import { ModalQuestion } from "../ModalQuestion.js/ModalQuestion";
+// import CardDesktop from "./CardDesktop";
+// import Carousel from "./Carousel";
+// import Question from "./Question";
+// import Titulo from "./Titulo";
 
-export const SectionDesktopCards = () => {
-  return (
-    <div className="row custom-desktop">
-      <ModalQuestion
-        content={
-          "Select the cards corresponding to the pack you want to hire and follow the buttons to continue the process!"
-        }
-      />
-      <div className="col-12 ">
-        <div className="col align-self-center">
-          <Titulo
-            title="DESIGN PACKS"
-            description="We design everything from the image of your business to the functionality of your website or software solution, to achieve an excellent experience for your users."
-          />
-        </div>
-      </div>
+// export const SectionDesktopCards = () => {
+//   return (
 
-      <div className="col-12 d-lg-none d-md-block">
-        <Carousel />
-      </div>
-      <div className="col align-self-start custom-question">
-        <Question />
-      </div>
-      <div className="col-12 d-none d-lg-block ">
-        <CardDesktop />
-      </div>
-    </div>
-  );
-};
+//   );
+// };
--- frontend/src/pages/marketinPage/MarketinPacksPage.js
+++ frontend/src/pages/marketinPage/MarketinPacksPage.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import { HeaderSub } from '../../components/DesingPageComponents/HeaderSub/HeaderSub';
-import { NextAndBack } from '../../components/DesingPageComponents/DownContainer/NextBack.js';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/DesingPageComponents/barComponent/Button";
+import { CircleBar } from "../../components/DesingPageComponents/barComponent/CircleBar";
+import CardDesktop from "../../components/DesingPageComponents/Cards/CardDesktop";
+import Carousel from "../../components/DesingPageComponents/Cards/Carousel";
+import Question from "../../components/DesingPageComponents/Cards/Question";
+import Titulo from "../../components/DesingPageComponents/Cards/Titulo";
+import { HeaderSub } from "../../components/DesingPageComponents/HeaderSub/HeaderSub";
+import { ModalQuestion } from "../../components/DesingPageComponents/ModalQuestion.js/ModalQuestion";
 
-export const MarketinPacksPage = ({setIsActiveNav}) => {
+import "./marketingPacks.css";
+export const MarketinPacksPage = () => {
   return (
     <div className="marketinPacksPage">
-       <HeaderSub setIsActiveNav={setIsActiveNav} />
-       <NextAndBack/>
+      <HeaderSub />
+      <div className="container custom-flexDesingpage  ">
+        <div className="row custom-desktop">
+          <ModalQuestion
+            content={
+              "Select the cards corresponding to the pack you want to hire and follow the buttons to continue the process!"
+            }
+          />
+          <div className="col-12 ">
+            <div className="col align-self-center">
+              <Titulo
+                title="marketingPacksPage"
+                description="We create and manage your presence in social networks and advertising, based on a rigorous study of the market and the user of your product!"
+              />
+            </div>
+          </div>
+
+          <div className="col-12 d-lg-none d-md-block">
+            <Carousel />
+          </div>
+          <div className="col align-self-start custom-question">
+            <Question />
+          </div>
+          <div className="col-12 d-none d-lg-block ">
+            <CardDesktop />
+          </div>
+        </div>
+      </div>
+      <div className="flex-bar">
+        <Link to="/development-packs-page">
+          <Button content="DESIGN PACKS" />
+        </Link>
+        <CircleBar />
+        <Link to="/payment-page">
+          <Button content="PAYMENT DETAILS" />
+        </Link>
+      </div>
     </div>
   );
 };
