88ed10474bae8b8254a36b3c2b63f193d2245f45
gonza <gonzalo.cubillas15@gmail.com>
2022-08-29 10:40:45 -0300

    modif carousel

--- frontend/src/components/DesingPageComponents/Cards/CarouselIem.js
+++ frontend/src/components/DesingPageComponents/Cards/CarouselIem.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const CarouselIem = ({clases,title,include, landing,price}) => {
+const CarouselIem = ({ clases, title, include, landing, price }) => {
   return (
     <div className={clases}>
-            <h2>{title}</h2>
-            <h3>{include}</h3>
-            <p>{landing}</p>
-            <hr />
-            <h3 className="precio">{price}</h3>
-          </div>
+      <h2>{title}</h2>
+      <h3>{include}</h3>
+      <p>{landing}</p>
+      <hr />
+      <h3 className="precio">{price}</h3>
+    </div>
+  );
+};
 
-  )
-}
-
-export default CarouselIem
+export default CarouselIem;
