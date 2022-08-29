010f6110a334e630b21393a8129bc838f97ee509
gonza <gonzalo.cubillas15@gmail.com>
2022-08-29 12:52:15 -0300

    ajuste de cards

--- frontend/src/components/DesingPageComponents/Cards/CarouselIem.js
+++ frontend/src/components/DesingPageComponents/Cards/CarouselIem.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const CarouselIem = ({ clases, title, include, landing, price }) => {
-
-
   return (
     <div className={clases}>
       <h2>{title}</h2>
       <h3>{include}</h3>
       <p>{landing}</p>
-    
-      <h3 className="precio">{price}</h3>
+      <div className="precio">
+      <h3 ><span> Chose :</span>{price}</h3>
+      </div>
+      
     </div>
   );
 };
