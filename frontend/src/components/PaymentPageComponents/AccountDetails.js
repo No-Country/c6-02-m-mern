1f58a2c3de59b8a572bf215e1cb3b961a3857ef7
unknown <santiagoschroh@gmail.com>
2022-09-01 11:16:54 -0300

    cssPaymentCorregido

--- frontend/src/components/PaymentPageComponents/AccountDetails.js
+++ frontend/src/components/PaymentPageComponents/AccountDetails.js
@@ -4,19 +4,26 @@ import "./Payment.css";
 export const AccountDetail = ({ metal, section, price, totalPrice }) => {
   return (
     <div className="accountDetails">
-      <h1> Account Details</h1>
+      <h1>
+        <span className="sizeAccountDetails">Account Details</span>
+      </h1>
 
       <div className="textContent">
-        <p>
-          {metal} {section} ... ${price}
+        <p className="classPacks">
+          {metal} {section} ...{" "}
+          <span className="colorPricePacks">${price}</span>
         </p>
-        <p>
-          {metal} {section} ... ${price}
+        <p className="classPacks">
+          {metal} {section} ...{" "}
+          <span className="colorPricePacks">${price}</span>
         </p>
-        <p>
-          {metal} {section} ... ${price}
+        <p className="classPacks">
+          {metal} {section} ...{" "}
+          <span className="colorPricePacks">${price}</span>
         </p>
-        <p className="total">total ......... ${totalPrice}</p>
+        <p className="total">
+          Total .... <span className="colorPrice">${totalPrice}</span>
+        </p>
       </div>
     </div>
   );
