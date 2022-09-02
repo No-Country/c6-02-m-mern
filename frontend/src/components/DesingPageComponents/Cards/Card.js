83904f7a89982bd78eda19d9eb2da74ab072100a
cristian mosquera <cmosqbone1@gmail.com>
2022-09-02 15:49:22 +0200

    adding card dinamic

--- frontend/src/components/DesingPageComponents/Cards/Card.js
+++ frontend/src/components/DesingPageComponents/Cards/Card.js
@@ -3,14 +3,14 @@ import "./Card.css";
 const Card = ({ titulo, include, page, precio, onActive, id }) => {
   return (
     <div className="card" onClick={onActive}>
-      <h3 className="card__title">BRONZE</h3>
-      <p className="card__includes-paragraph">INCLUDES</p>
+      <h3 className="card__title">{titulo}</h3>
+      <p className="card__includes-paragraph">{include}</p>
 
       <p className="card__description">Landing page desing</p>
       <div className="card__wrapper-price">
         <p className="card__wrapper__chose">chose</p>
         <p className="card__wrapper__exchange">$</p>
-        <p className="card__wrapper__price">1000</p>
+        <p className="card__wrapper__price">{precio}</p>
       </div>
     </div>
   );
